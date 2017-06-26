f154fcf11a6ccf93d9fa2bf2c8187fc2b0c162b7
Adam Becker <ad@mbecker.cc>
2017-06-26 14:48:35 -0700

    add router, a form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,110 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-class App extends Component {
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Adam',
+      submitted: false
+    }
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.setState({name: e.target.value, submitted: false});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({submitted: true});
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.name} type="text" onChange={this.handleNameChange} />
+          <button>Submit</button>
+        </form>
+
+        {
+          this.state.submitted &&
+          <h2>Hello, {this.state.name}!</h2>
+        }
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+        <li><Link to="/form">The greatest form</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+      <Route path="/form" component={Form}/>
+    </div>
+  </Router>
+)
+export default BasicExample
